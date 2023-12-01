d6fd4b8bd7a9e740343c5df739d04ad49c500717
Abhinay D <92934353+abhinayrd@users.noreply.github.com>
2023-12-01 16:33:59 +0530

    The file has been updated to incorporate modularity.

--- javexample.java
+++ javexample.java
@@ -1,15 +1,13 @@
 class javexample{  
- public static void main(String args[])  
- {    
-  Scanner sc = new Scanner(System.in);
-  int n = sc.nextInt();
-  int arr[] = new int[n];
-  int res = 0;
-  for(int i = 0 ; i < n ; i++){
-   arr[i] = sc.nextInt();
-   if(i == 0) res = arr[i];
-   res = Math.max(res, arr[i]);
-  }
-  System.out.println(res);
- }
+    public static void main(String args[]){    
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println(getMaximum(arr));
+     }
+    int getMaximum(int arr[]){
+        int res = arr[0];
+        for(int i : arr) res = Math.max(res, i);
+        return res;
+    }
 }  
