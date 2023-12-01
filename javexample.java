e095f7a3da2c67dedee755d3cb8b413bd97b9984
Abhinay D <92934353+abhinayrd@users.noreply.github.com>
2023-12-01 16:14:06 +0530

    Changed the program to find maximum number in an array.

--- javexample.java
+++ javexample.java
@@ -1,15 +1,14 @@
 class javexample{  
-public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ public static void main(String args[])  
  {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }    
-  
-}}  
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  int arr[] = new int[n];
+  int res = 0;
+  for(int i = 0 ; i < n ; i++){
+   arr[i] = sc.nextInt();
+   res = Math.max(res, arr[i]);
+  }
+  System.out.println(res);
+ }
+}  
